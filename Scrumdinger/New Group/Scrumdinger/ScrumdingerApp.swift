dafa3834f9bc95c235dbead548dc1b1f7522a882
Rone Chen <m13552079019@163.com>
2024-12-15 15:44:30 +0800

    new theme

---
+++ Scrumdinger/New Group/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Rong Chen on 2024/12/15.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
